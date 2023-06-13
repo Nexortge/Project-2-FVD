473ad59f10b913e0d15de36e79704491487f2a52
mark <markn@mark>
2023-06-13 14:38:41 +0200

    css

--- html/login.php
+++ html/login.php
@@ -1,28 +1,51 @@
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
     <title>Login</title>
 </head>
+
 <body>
-    <div class="form-container">
-        <form>
-            <label>Voornaam*:</label>
-            <input type="text" maxlength="32" required> 
+    <div class="grid-container">
+        <header>
+            <img src="../assets/FVD.png" alt="">
+            <h1>Forum Voor Democratie</h1>
+        </header>
 
-            <label>Achternaam*:</label>
-            <input type="text" maxlength="32" required>
+        <div class="form-container">
+            <form>
+                <label>Voornaam*</label>
+                <input type="text" maxlength="32" required>
 
-            <label>Email*:</label>
-            <input type="mail" maxlength="32" required>
+                <label>Achternaam*</label>
+                <input type="text" maxlength="32" required>
 
-            <label>Wachtwoord*:</label>
-            <input type="password" maxlength="32" required>
-            
-            <label>Telefoonnummer:</label>
-            <input type="number" maxlength="15">
-        </form>
+                <label>Email*</label>
+                <input type="mail" maxlength="32" required>
+
+                <label>Wachtwoord*</label>
+                <input type="password" maxlength="32" required>
+
+                <label>Telefoonnummer</label>
+                <input type="number" maxlength="15">
+            </form>
+        </div>
+
+        <button>Sluit je aan!</button>
+        <button>of login</button>
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m15.386.524c-4.764 0-8.64 3.876-8.64 8.64 0 4.75 3.876 8.613 8.64 8.613 4.75 0 8.614-3.864 8.614-8.613 0-4.764-3.864-8.64-8.614-8.64m-15.383 23.013h4.22v-23.013h-4.22"/></svg>
+
+        <footer>
+            <img src="../assets/FVD.png" alt="" height="60">
+            <button>Wordt lid</button>
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)"/><path d="m17 3.5a.5.5 0 0 0 -.5-.5h-2.5a4.77 4.77 0 0 0 -5 4.5v2.7h-2.5a.5.5 0 0 0 -.5.5v2.6a.5.5 0 0 0 .5.5h2.5v6.7a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-6.7h2.62a.5.5 0 0 0 .49-.37l.72-2.6a.5.5 0 0 0 -.48-.63h-3.35v-2.7a1 1 0 0 1 1-.9h2.5a.5.5 0 0 0 .5-.5z" fill="#231f20"/></svg>
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.53.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z"/></svg>
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m0 0h24v24h-24" fill="#fff" opacity="0"/><path d="m8.08 20a11.07 11.07 0 0 0 11.44-11 8.09 8.09 0 0 0 1.48-2.84.44.44 0 0 0 -.62-.51 1.88 1.88 0 0 1 -2.16-.38 3.89 3.89 0 0 0 -5.58-.17 4.13 4.13 0 0 0 -1.15 3.9c-3.35.2-5.65-1.39-7.49-3.57a.43.43 0 0 0 -.75.24 9.68 9.68 0 0 0 4.6 10.05 6.73 6.73 0 0 1 -4.47 2.28.45.45 0 0 0 -.14.84 11 11 0 0 0 4.84 1.16" fill="#231f20"/></svg>
+        </footer>
     </div>
 </body>
+
 </html>
