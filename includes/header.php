a7f5804b7c7ab9c1088a161975ca01c7d11c5dfd
Nexortge <ps240569@summacollege.nl>
2023-06-16 10:20:18 +0200

    Header v1

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="../css/includes.css">
+<link rel="stylesheet" href="/css/includes.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+<div class="header">
+    <div style="display: flex;">
+        <img id="logo-img" src="../assets/FVD.png" alt="logo">
+        <h1>Forum voor democratie</h1>
+    </div>
+
+    <div id="patreon-logo">
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="m15.386.524c-4.764 0-8.64 3.876-8.64 8.64 0 4.75 3.876 8.613 8.64 8.613 4.75 0 8.614-3.864 8.614-8.613 0-4.764-3.864-8.64-8.614-8.64m-15.383 23.013h4.22v-23.013h-4.22" />
+        </svg>
+        <p>Subscribe</p>
+    </div>
+
+</div>
