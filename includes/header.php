238020efe3685418daf4da27d9590abc1adf8063
mark <markn@mark>
2023-06-21 10:18:41 +0200

    a href header.php en map html > php

--- includes/header.php
+++ includes/header.php
@@ -27,12 +27,14 @@ function isCurrentPage($page)
         <img id="logo-img" src="../assets/FVD.png" alt="logo">
         <h1>Forum voor democratie</h1>
     </div>
-    
+
     <div id="patreon-logo">
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="m15.386.524c-4.764 0-8.64 3.876-8.64 8.64 0 4.75 3.876 8.613 8.64 8.613 4.75 0 8.614-3.864 8.614-8.613 0-4.764-3.864-8.64-8.614-8.64m-15.383 23.013h4.22v-23.013h-4.22" />
-        </svg>
-        <p>Subscribe</p>
+        <a href="https://www.patreon.com/">
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="m15.386.524c-4.764 0-8.64 3.876-8.64 8.64 0 4.75 3.876 8.613 8.64 8.613 4.75 0 8.614-3.864 8.614-8.613 0-4.764-3.864-8.64-8.614-8.64m-15.383 23.013h4.22v-23.013h-4.22" />
+            </svg>
+            <p>Subscribe</p>
+        </a>
     </div>
 
 
