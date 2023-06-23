337ca5a04ae11c742198942d9929dbcdb52cc256
Nexortge <ps240569@summacollege.nl>
2023-06-23 14:48:21 +0200

    niewsbrief toegevoegd

---
+++ php/nieuws.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/nieuws.css">
+    <title>FVD</title>
+</head>
+
+<body>
+    <?php include '../includes/header.php'; ?>
+    <h1 class="title">Nieuws brief</h1>
+    <div class="card-holder">
+        <div class="card">
+            <div class="card-padding">
+                <img src="../assets/standpunt.webp" alt="img1">
+                <div style="display: flex; align-items: center; justify-content:space-between;">
+                    <h1>Big blue balls</h1>
+                    <p>door balzak</p>
+                </div>
+
+                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
+                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
+                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
+                    quam tem por phasellus. Aenean varius est hac </p>
+                <a href="https://poep.nl">lees verder...</a>
+
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="card-padding">
+                <img src="../assets/standpunt.webp" alt="img1">
+                <div style="display: flex; align-items: center; justify-content:space-between;">
+                    <h1>Big blue balls</h1>
+                    <p>door balzak</p>
+                </div>
+
+                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
+                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
+                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
+                    quam tem por phasellus. Aenean varius est hac </p>
+                <a href="https://poep.nl">lees verder...</a>
+
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="card-padding">
+                <img src="../assets/standpunt.webp" alt="img1">
+                <div style="display: flex; align-items: center; justify-content:space-between;">
+                    <h1>Big blue balls</h1>
+                    <p>door balzak</p>
+                </div>
+
+                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
+                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
+                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
+                    quam tem por phasellus. Aenean varius est hac </p>
+                <a href="https://poep.nl">lees verder...</a>
+
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="card-padding">
+                <img src="../assets/standpunt.webp" alt="img1">
+                <div style="display: flex; align-items: center; justify-content:space-between;">
+                    <h1>Big blue balls</h1>
+                    <p>door balzak</p>
+                </div>
+
+                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
+                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
+                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
+                    quam tem por phasellus. Aenean varius est hac </p>
+                <a href="https://poep.nl">lees verder...</a>
+
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="card-padding">
+                <img src="../assets/standpunt.webp" alt="img1">
+                <div style="display: flex; align-items: center; justify-content:space-between;">
+                    <h1>Big blue balls</h1>
+                    <p>door balzak</p>
+                </div>
+
+                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
+                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
+                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
+                    quam tem por phasellus. Aenean varius est hac </p>
+                <a href="https://poep.nl">lees verder...</a>
+
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="card-padding">
+                <img src="../assets/standpunt.webp" alt="img1">
+                <div style="display: flex; align-items: center; justify-content:space-between;">
+                    <h1>Big blue balls</h1>
+                    <p>door balzak</p>
+                </div>
+
+                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
+                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
+                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
+                    quam tem por phasellus. Aenean varius est hac </p>
+                <a href="https://poep.nl">lees verder...</a>
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+    <?php include '../includes/footer.php'; ?>
+</body>
+
+</html>
