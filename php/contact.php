89751f284171b63173420826fc32a183ffa6d688
mark <markn@mark>
2023-06-23 11:49:02 +0200

    index en contact

---
+++ php/contact.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/contact.css">
+    <title>FVD</title>
+</head>
+
+<body>
+    <?php
+    include '../includes/header.php';
+    ?>
+    <h1 id="fvd-head">Contact</h1>
+    <p id="fvd-head-text">Het FVD-partijbureau staat klaar voor al uw vragen over het lidmaatschap en evenementen. Heeft u een politieke vraag, neem dan contact op de Tweede Kamerfractie.</p>
+    <div class="fvd-bureau">
+        <div>
+            <h3>FVD-Partijbureau</h3>
+            <div class="tijden">09:00 – 17:00 (ma t/m vr)</div>
+            <div>
+                <a href="tel:020 - 261 2874">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd"></path>
+                    </svg>
+                    <div>020 - 261 2874</div>
+                </a>
+                <a href="mailto:partij@fvd.nl">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"></path>
+                    </svg>
+                    <div>partij@fvd.nl</div>
+                </a>
+            </div>
+        </div>
+        <div>
+            <h3>Tweede Kamerfractie FVD</h3>
+            <div class="tijden">13:00 – 17:00 (di t/m do)</div>
+            <div>
+                <a href="tel:070 - 318 3121">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-5 w-5">
+                        <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd"></path>
+                    </svg>
+                    <div>070 - 318 3121</div>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+<?php
+include '../includes/footer.php';
+?>
--- php/index.php
+++ php/index.php
@@ -52,59 +52,52 @@
     </div>
 
     <div class="slideshow-container">
+        <!-- Full-width images with number and caption text -->
+        <div class="mySlides fade">
+            <img src="../assets/freek-vonk.jpg">
+            <div class="slider-text">Freek Vonk</div>
+        </div>
 
-<!-- Full-width images with number and caption text -->
-<div class="mySlides fade">
-  <div class="numbertext">1 / 6</div>
-  <img src="../assets/freek-vonk.jpg" style="width:100%">
-  <div class="text">Freek Vonk</div>
-</div>
+        <div class="mySlides fade">
+            <img src="../assets/thierry-baudet.webp">
+            <div class="slider-text">Thierry Baudet</div>
+        </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">2 / 6</div>
-  <img src="../assets/thierry-baudet.webp" style="width:100%">
-  <div class="text">Thierry Baudet</div>
-</div>
+        <div class="mySlides fade">
+            <img src="../assets/gideon-van-meijeren.webp">
+            <div class="slider-text">Gideon van Meijeren</div>
+        </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">3 / 6</div>
-  <img src="../assets/gideon-van-meijeren.webp" style="width:100%">
-  <div class="text">Gideon van Meijeren</div>
-</div>
+        <div class="mySlides fade">
+            <img src="../assets/freek-jansen.webp">
+            <div class="slider-text">Freek Jansen</div>
+        </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">4 / 6</div>
-  <img src="../assets/freek-jansen.webp" style="width:100%">
-  <div class="text">Freek Jansen</div>
-</div>
+        <div class="mySlides fade">
+            <img src="../assets/pepijn-van-houwelingen.webp">
+            <div class="slider-text">Pepijn van Houwelingen</div>
+        </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">5 / 6</div>
-  <img src="../assets/pepijn-van-houwelingen.webp" style="width:100%">
-  <div class="text">Pepijn van Houwelingen</div>
-</div>
+        <div class="mySlides fade">
+            <img src="../assets/simone-kerseboom.webp">
+            <div class="slider-text">Simone Kerseboom</div>
+        </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">6 / 6</div>
-  <img src="../assets/simone-kerseboom.webp" style="width:100%">
-  <div class="text">Simone Kerseboom</div>
-</div>
+        <!-- Next and previous buttons -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+    <br>
 
-<!-- Next and previous buttons -->
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
-</div>
-<br>
-
-<!-- The dots/circles -->
-<div style="text-align:center">
-<span class="dot" onclick="currentSlide(1)"></span>
-<span class="dot" onclick="currentSlide(2)"></span>
-<span class="dot" onclick="currentSlide(3)"></span>
-<span class="dot" onclick="currentSlide(4)"></span>
-<span class="dot" onclick="currentSlide(5)"></span>
-<span class="dot" onclick="currentSlide(6)"></span>
-</div>
+    <!-- The dots/circles -->
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+        <span class="dot" onclick="currentSlide(4)"></span>
+        <span class="dot" onclick="currentSlide(5)"></span>
+        <span class="dot" onclick="currentSlide(6)"></span>
+    </div>
 </body>
 
 </html>
