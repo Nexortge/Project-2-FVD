2c47ccaa93c428fd457f87d6c88047fb618452cf
Nexortge <ps240569@summacollege.nl>
2023-06-27 12:38:17 +0200

    standpunten 3/4

--- php/stand.php
+++ php/stand.php
@@ -12,104 +12,107 @@
     <?php include '../includes/header.php'; ?>
     <h1 id="title">Dit zijn onze standpunten.</h1>
 
-    <div class="card-holder">
-        <div class="card top-left">
-            <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                    <circle cx="12" cy="12" r="10" />
-                    <path d="m3 12h19" />
-                    <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
-                    <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
-                </g>
-            </svg>
-            <h2>Soevereiniteit1</h2>
+    <div id="card-holder-holder">
+        <div class="card-holder">
+            <div class="card top-left right" id="card-1">
+                <svg style="z-index: 3;" fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                        <circle cx="12" cy="12" r="10" />
+                        <path d="m3 12h19" />
+                        <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
+                        <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
+                    </g>
+                </svg>
+                <h2 style="z-index: 3;" >Soevereiniteit</h2>
 
-            <p>
-                Een sterke Europese Unie is goed voor Nederland. Europa moet doen waar het goed in is: het oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
-            </p>
-        </div>
+                <p style="z-index: 3;" >
+                Soevereiniteit vormt het uitgangspunt van een democratie. Zonder soeverein parlement dat zélf beslissingen kan nemen, valt er voor de bevolking immers ook niets te kiezen. De afgelopen decennia is een aantal supranationale organisaties opgericht die onze soevereiniteit ernstig hebben ingeperkt. Daarmee is ons vermogen om zelf te beslissen over - bijvoorbeeld - immigratie, volksgezondheid, stikstofnormen en rentevoet ernstig aangetast.
+                </p>
+            </div>
 
-        <div class="card">
-            <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                    <circle cx="12" cy="12" r="10" />
-                    <path d="m3 12h19" />
-                    <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
-                    <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
-                </g>
-            </svg>
-            <h2>Soevereiniteit2</h2>
+            <div class="card right" id="card-2">
+                <svg style="z-index: 3;"  fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                        <circle cx="12" cy="12" r="10" />
+                        <path d="m3 12h19" />
+                        <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
+                        <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
+                    </g>
+                </svg>
+                <h2 style="z-index: 3;" >Soevereiniteit2</h2>
 
-            <p>
-                Een sterke Europear het goed in is: het oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
-            </p>
-        </div>
+                <p style="z-index: 3;" >
+                    Een sterke Europear het goed in is: het oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
+                </p>
+            </div>
 
 
-        <div class="card top-right">
-            <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                    <circle cx="12" cy="12" r="10" />
-                    <path d="m3 12h19" />
-                    <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
-                    <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
-                </g>
-            </svg>
-            <h2>Soevereiniteit3</h2>
+            <div class="card top-right" id="card-3">
+                <svg  style="z-index: 3;" fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                        <circle cx="12" cy="12" r="10" />
+                        <path d="m3 12h19" />
+                        <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
+                        <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
+                    </g>
+                </svg>
+                <h2 style="z-index: 3;" >Soevereiniteit3</h2>
 
-            <p>
-                Een sterke Europese Unie is goed voor Nederland. Eet doen waar het goed in is: het oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
-            </p>
-        </div>
+                <p style="z-index: 3;" >
+                    Een sterke Europese Unie is goed voor Nederland. Eet doen waar het goed in is: het oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
+                </p>
+            </div>
 
-        <div class="card bottom-left">
-            <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                    <circle cx="12" cy="12" r="10" />
-                    <path d="m3 12h19" />
-                    <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
-                    <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
-                </g>
-            </svg>
-            <h2>Soevereiniteit4</h2>
+            <div class="card bottom-left right top" id="card-4">
+                <svg style="z-index: 3;"  fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                        <circle cx="12" cy="12" r="10" />
+                        <path d="m3 12h19" />
+                        <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
+                        <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
+                    </g>
+                </svg>
+                <h2 style="z-index: 3;" >Soevereiniteit4</h2>
 
-            <p>
-                Een sterke Europese Unie iopa moet doen waar het goed in is: het oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
-            </p>
-        </div>
+                <p style="z-index: 3;" >
+                    Een sterke Europese Unie iopa moet doen waar het goed in is: het oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
+                </p>
+            </div>
 
-        <div class="card">
-            <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                    <circle cx="12" cy="12" r="10" />
-                    <path d="m3 12h19" />
-                    <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
-                    <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
-                </g>
-            </svg>
-            <h2>Soevereiniteit5</h2>
+            <div class="card right top" id="card-5">
+                <svg style="z-index: 3;"  fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                        <circle cx="12" cy="12" r="10" />
+                        <path d="m3 12h19" />
+                        <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
+                        <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
+                    </g>
+                </svg>
+                <h2 style="z-index: 3;" >Soevereiniteit5</h2>
 
-            <p>
-                Een sterke Europese Unie is goed voor Nederland. Europa mo oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
-            </p>
-        </div>
+                <p style="z-index: 3;" >
+                    Een sterke Europese Unie is goed voor Nederland. Europa mo oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
+                </p>
+            </div>
 
-        <div class="card bottom-right">
-            <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                    <circle cx="12" cy="12" r="10" />
-                    <path d="m3 12h19" />
-                    <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
-                    <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
-                </g>
-            </svg>
-            <h2>Soevereiniteit6</h2>
+            <div class="card bottom-right top" id="card-6">
+                <svg style="z-index: 3;"  fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                        <circle cx="12" cy="12" r="10" />
+                        <path d="m3 12h19" />
+                        <path d="m12 2.2019c2.4744 2.52508 4 5.98336 4 9.798 0 3.8146-1.5256 7.2729-4 9.7979" />
+                        <path d="m12 2.2019c-2.47437 2.52508-4 5.98336-4 9.798 0 3.8146 1.52563 7.2729 4 9.7979" />
+                    </g>
+                </svg>
+                <h2 style="z-index: 3;" >Soevereiniteit6</h2>
 
-            <p>
-                Een sterke Europese Unie is goed voor Nederland. Europa mo oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
-            </p>
+                <p>
+                    Een sterke Europese Unie is goed voor Nederland. Europa mo oplossen van problemen die landen niet in hun eentje aankunnen. Naar de rest van de wereld kijken wij met een realistische blik. Onze geweldige militairen zijn er primair om het NAVO-grondgebied veilig te houden. Wij sturen hen alleen op missies die de Nederlandse belangen verdedigen. Daarnaast willen wij onze economische afhankelijkheid van China
+                </p>
+            </div>
         </div>
     </div>
+
     <?php include '../includes/footer.php'; ?>
 
 
