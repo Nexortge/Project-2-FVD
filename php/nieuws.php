518c2f62585132523425c3a4895f35f3c770c362
mark <markn@mark>
2023-06-28 10:28:23 +0200

    nieuws pagina compleet

--- php/nieuws.php
+++ php/nieuws.php
@@ -11,106 +11,88 @@
 <body>
     <?php include '../includes/header.php'; ?>
     <h1 class="title">Nieuws brief</h1>
-    
+
     <div class="card-holder">
         <div class="card">
             <div class="card-padding">
-                <img src="../assets/standpunt.webp" alt="img1">
+                <img src="../assets/freek-vonk.jpg" alt="img1">
                 <div style="display: flex; align-items: center; justify-content:space-between;">
-                    <h1>Big blue balls</h1>
-                    <p>door balzak</p>
+                    <h1>Freek Vonk haalt € 10.000,- op voor Indisch Platform</h1>
+                    <p>door FVD</p>
                 </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
-                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
-                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
-                    quam tem por phasellus. Aenean varius est hac </p>
-                <a href="https://poep.nl">lees verder...</a>
+                <p>Vandaag ontving FVD-partijleider de slachtoffers van de Japanse bezetting in Nederlands-Indië in de Tweede Kamer. De Nederlandse overheid geeft deze moedige mensen niet het geld waar ze recht op hebben. Het is hartverscheurend.</p>
+                <a href="https://fvd.nl/nieuws/fvd-haalt-10000-op-voor-indisch-platform">lees verder...</a>
 
             </div>
 
         </div>
         <div class="card">
             <div class="card-padding">
-                <img src="../assets/standpunt.webp" alt="img1">
+                <img src="../assets/fvd-crowdfunding.webp" alt="img1">
                 <div style="display: flex; align-items: center; justify-content:space-between;">
-                    <h1>Big blue balls</h1>
-                    <p>door balzak</p>
+                    <h1>FVD start crowdfunding!</h1>
+                    <p>door FVD</p>
                 </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
-                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
-                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
-                    quam tem por phasellus. Aenean varius est hac </p>
-                <a href="https://poep.nl">lees verder...</a>
+                <p>Het gaat goed met de Renaissanceschool. Dankzij een donateur is het mogelijk geworden om een klas met meer dan 10 kinderen geheel kosteloos onderwijs aan te bieden.</p>
+                <a href="https://fvd.nl/nieuws/help-de-renaissanceschool-vooruit">lees verder...</a>
 
             </div>
 
         </div>
         <div class="card">
             <div class="card-padding">
-                <img src="../assets/standpunt.webp" alt="img1">
+                <img src="../assets/Eerste-kamer.webp" alt="img1">
                 <div style="display: flex; align-items: center; justify-content:space-between;">
-                    <h1>Big blue balls</h1>
-                    <p>door balzak</p>
+                    <h1>Nieuwe Eerste Kamer geïnstalleerd</h1>
+                    <p>door FVD</p>
                 </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
-                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
-                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
-                    quam tem por phasellus. Aenean varius est hac </p>
-                <a href="https://poep.nl">lees verder...</a>
+                <p>De nieuwe Eerste Kamer is geïnstalleerd. Na de Provinciale Statenverkiezingen op 15 maart hebben alle Statenleden van Nederland hun stem uitgebracht voor de nieuwe senaat. Forum voor Democratie bekleedt nu twee zetels in de Eerste Kamer.</p>
+                <a href="https://fvd.nl/nieuws/nieuwe-eerste-kamer-geinstalleerd">lees verder...</a>
 
             </div>
 
         </div>
         <div class="card">
             <div class="card-padding">
-                <img src="../assets/standpunt.webp" alt="img1">
+                <img src="../assets/american-hegemony.webp" alt="img1">
                 <div style="display: flex; align-items: center; justify-content:space-between;">
-                    <h1>Big blue balls</h1>
-                    <p>door balzak</p>
+                    <h1>LGBT - a weapon for American Hegemony</h1>
+                    <p>door John Laughland</p>
                 </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
-                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
-                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
-                    quam tem por phasellus. Aenean varius est hac </p>
-                <a href="https://poep.nl">lees verder...</a>
+                <p>One of Joe Biden’s first acts as president of the United States was to sign, within two weeks of his inauguration, a “Memorandum on Advancing the Human Rights of Lesbian, Gay, Bisexual, Transgender, Queer, and Intersex Persons Around the World.”</p>
+                <a href="https://fvd.nl/nieuws/lgbt-a-weapon-for-american-hegemony">lees verder...</a>
 
             </div>
 
         </div>
         <div class="card">
             <div class="card-padding">
-                <img src="../assets/standpunt.webp" alt="img1">
+                <img src="../assets/fvd-raadslid.webp" alt="img1">
                 <div style="display: flex; align-items: center; justify-content:space-between;">
-                    <h1>Big blue balls</h1>
-                    <p>door balzak</p>
+                    <h1>FVD-raadslid woord ontnomen</h1>
+                    <p>door FVD</p>
                 </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
-                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
-                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
-                    quam tem por phasellus. Aenean varius est hac </p>
-                <a href="https://poep.nl">lees verder...</a>
+                <p>Opnieuw wordt een FVD-raadslid het woord ontnomen tijdens een kritisch betoog. In Sittard-Geleen schorste GroenLinks-voorzitter De Loo de vergadering toen hij vond dat Amira Lotfy te lang aan het woord was.</p>
+                <a href="https://fvd.nl/nieuws/fvd-raadslid-woord-ontnomen-tijdens-kritisch-betoog-over-plaatsing-cno">lees verder...</a>
 
             </div>
 
         </div>
         <div class="card">
             <div class="card-padding">
-                <img src="../assets/standpunt.webp" alt="img1">
+                <img src="../assets/who-verdrag.webp" alt="img1">
                 <div style="display: flex; align-items: center; justify-content:space-between;">
-                    <h1>Big blue balls</h1>
+                    <h1>Pepijn van Houwelingen dwingt kabinet</h1>
                     <p>door balzak</p>
                 </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur. Viverra id amet in odio enim venenatis nulla.
-                    Sit lorem pellentesque egestas eget pretium mattis. Neque risus vulputate vitae nec lectus commodo
-                    eget proin purus. Eget nisl sit auctor ullamcorper. Odio euismod vestibulum mi pulvinar. Porta vitae
-                    quam tem por phasellus. Aenean varius est hac </p>
-                <a href="https://poep.nl">lees verder...</a>
+                <p>Een belangrijke overwinning in onze vrijheidsstrijd: FVD-Tweede Kamerlid Pepijn van Houwelingen heeft afgedwongen dat het nieuwe WHO-verdrag, dat zeer omstreden is vanwege de inbreuk op onze soevereiniteit, eerst ter goedkeuring aan de Tweede Kamer moet worden voorgelegd. Daarvoor verzamelde hij de vereiste steun van ten minste 30 Tweede Kamerleden.</p>
+                <a href="https://fvd.nl/nieuws/pepijn-van-houwelingen-dwingt-kabinet-tot-uitstel-who-verdrag">lees verder...</a>
 
             </div>
 
