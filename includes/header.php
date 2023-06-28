5a78a7c1aab5f846acc62f5e03f5410b214a4f8d
Nexortge <ps240569@summacollege.nl>
2023-06-28 10:59:58 +0200

    hamburger icon barebones

--- includes/header.php
+++ includes/header.php
@@ -16,35 +16,45 @@ function isCurrentPage($page)
 }
 ?>
 
+<body>
+    <script src="../js/menu.js" defer></script>
+    <script src="/js/menu.js" defer></script>
+    <link rel="stylesheet" href="../css/includes.css">
+    <link rel="stylesheet" href="/css/includes.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <div class="header">
+        <div class="logo-text">
+            <img id="logo-img" src="../assets/FVD.png" alt="logo">
+            <h1>Forum voor democratie</h1>
+        </div>
 
-<link rel="stylesheet" href="../css/includes.css">
-<link rel="stylesheet" href="/css/includes.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
-<div class="header">
-    <div class="logo-text">
-        <img id="logo-img" src="../assets/FVD.png" alt="logo">
-        <h1>Forum voor democratie</h1>
+        <div id="patreon-logo">
+            <a href="https://www.patreon.com/">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="m15.386.524c-4.764 0-8.64 3.876-8.64 8.64 0 4.75 3.876 8.613 8.64 8.613 4.75 0 8.614-3.864 8.614-8.613 0-4.764-3.864-8.64-8.614-8.64m-15.383 23.013h4.22v-23.013h-4.22" />
+                </svg>
+                <p>Subscribe</p>
+            </a>
+        </div>
+
+
+
+    </div>
+    <div id="menu-items">
+        <ul>
+            <li class="<?php echo isCurrentPage('index.php'); ?>"><a href="index.php">Home</a></li>
+            <li class="<?php echo isCurrentPage('donaties.php'); ?>"><a href="donaties.php">Donaties</a></li>
+            <li class="<?php echo isCurrentPage('contact.php'); ?>"><a href="contact.php">Contact</a></li>
+            <li class="<?php echo isCurrentPage('nieuws.php'); ?>"><a href="nieuws.php">Nieuwsbrief</a></li>
+            <li class="<?php echo isCurrentPage('stand.php'); ?>"><a href="stand.php">Standpunten</a></li>
+        </ul>
     </div>
 
-    <div id="patreon-logo">
-        <a href="https://www.patreon.com/">
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="m15.386.524c-4.764 0-8.64 3.876-8.64 8.64 0 4.75 3.876 8.613 8.64 8.613 4.75 0 8.614-3.864 8.614-8.613 0-4.764-3.864-8.64-8.614-8.64m-15.383 23.013h4.22v-23.013h-4.22" />
-            </svg>
-            <p>Subscribe</p>
-        </a>
+    <div class="hamburger-menu">
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
-
-
-</div>
-<div id="menu-items">
-    <ul>
-        <li class="<?php echo isCurrentPage('index.php'); ?>"><a href="index.php">Home</a></li>
-        <li class="<?php echo isCurrentPage('donaties.php'); ?>"><a href="donaties.php">Donaties</a></li>
-        <li class="<?php echo isCurrentPage('contact.php'); ?>"><a href="contact.php">Contact</a></li>
-        <li class="<?php echo isCurrentPage('nieuws.php'); ?>"><a href="nieuws.php">Nieuwsbrief</a></li>
-        <li class="<?php echo isCurrentPage('stand.php'); ?>"><a href="stand.php">Standpunten</a></li>
-    </ul>
-</div>
+</body>
